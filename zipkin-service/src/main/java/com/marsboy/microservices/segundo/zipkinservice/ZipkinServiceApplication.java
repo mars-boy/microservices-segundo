84036b362506d8fc6f157d3df937c9f30e45b17e
mars-boy <akhilvantipalli@gmail.com>
2018-03-02 21:55:17 +0530

    Fixed Zipkin traces

--- zipkin-service/src/main/java/com/marsboy/microservices/segundo/zipkinservice/ZipkinServiceApplication.java
+++ zipkin-service/src/main/java/com/marsboy/microservices/segundo/zipkinservice/ZipkinServiceApplication.java
@@ -2,13 +2,23 @@ package com.marsboy.microservices.segundo.zipkinservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.context.annotation.Bean;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinServer
+/*@EnableZipkinServer*/
+@EnableZipkinStreamServer
 public class ZipkinServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZipkinServiceApplication.class, args);
 	}
+
+	/*@Bean
+	public AlwaysSampler defaultSampler() {
+		return new AlwaysSampler();
+	}*/
+
 }
